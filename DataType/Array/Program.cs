da85f0030169b414e2df7941af345ae5fdaffe8a
KIMHYEOK\borad <kimmang940715@naver.com>
2025-01-03 00:03:26 +0900

    시작하세요 C# 12

---
+++ DataType/Array/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] products = new int[5];
+
+            string[] names = new string[1000];
+
+            products[0] = 100;
+            products[1] = 200;
+
+            int book = products[0];
+            int sum = products[0] + products[1];
+
+            string text = "Hello World";
+            char ch1 = text[0];
+            char ch2 = text[1];
+
+            Console.WriteLine(ch1);
+            Console.WriteLine(ch2);
+
+            // 다차원 배열
+
+            int[,] arr2 = new int[10, 5]; // 2차원 배열
+            short[,,] arr3 = new short[8, 3, 10]; // 3차원 배열
+
+            int[,] arr4 = new int[2, 3]
+            { 
+                { 1, 2, 3 },
+                { 4, 5, 6 }
+            };
+
+            int[,,] arr5 = new int[2, 3, 4]
+            {
+                {
+                    {1,2,3,4 },
+                    {5,6,7,8 },
+                    {9,10,11,12 },
+                },
+
+                {
+                {13,14,15,16 },
+                {17,18,19,20 },
+                {21,22,23,24 },
+                }
+            };
+        
+            
+        }
+    }
+}
--- DataType/ConsoleApp1/Program.cs
+++ DataType/ConsoleApp1/Program.cs
@@ -10,6 +10,56 @@ namespace DataType
     {
         static void Main(string[] args)
         {
+            // 정수형 기본 타입
+            int n1 = 50; // 선언과 동시에 값을 부여할 수도 있고
+            int n2; // 선언만 하고
+            n2 = 100; // 값은 나중에 부여하는 것도 가능하다
+
+            long sum = n1 + n2; // 더한 값을 저장
+            Console.WriteLine(sum); // 출력 결과 150
+
+            int n3 = 50;
+            n3 = 200;
+            Console.WriteLine(n3); // 출력 결과 : 200
+
+            System.Int32 n4 = 50;
+            Console.WriteLine(n4);
+
+            // 실수형 기본 타입
+            float f = 5.2f;
+            double d = 10.5;
+            decimal money = 200.099m;
+
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(money);
+
+            // 문자형 기본 타입
+            char ch = 'A';
+            Console.WriteLine(ch);
+
+            char ch1 = '\t'; // Tab 문자를 표현
+            char ch2 = 'T';
+            char ch3 = '\n'; // 개행(New Line) 문자를 표현
+            char ch4 = 'o';
+
+            char ch5 = '\u25b6';
+            Console.WriteLine(ch);
+
+            // 문자열 타입
+            string text = "Hello World";
+            Console.WriteLine(text);
+
+            // 불린(boolean)형 기본타입
+            bool isNumeric = false;
+
+
+            // 형변환
+            byte b = 250;
+            short s = b;
+
+            Console.WriteLine(s);
+            Console.WriteLine(s.GetType());
         }
     }
 }
